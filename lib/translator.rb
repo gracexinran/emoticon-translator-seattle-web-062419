2d31bbd3df6f1abb2c3a08d6b4f7963a388348ba
Xinran Che <gracexinran@gmail.com>
2019-05-23 20:56:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,15 @@
 # require modules here
 
-def load_library
+def load_library(file_path)
   # code goes here
+  hash = {}
+  hash['get_meaning'] = {}
+  hash['get_emoticon'] = {}
+  file_path.each do |k,v|
+  hash['get_meaning'][k] = v
+  hash['get_emoticon'][k] = v
+end
+  hash
 end
 
 def get_japanese_emoticon
